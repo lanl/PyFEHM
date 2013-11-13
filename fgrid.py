some fixes to geom coef calculation for stor file writing

fgrid.py
@@ -25,6 +25,8 @@ import numpy as np
 import os,math,platform,string,subprocess,shutil
 from subprocess import Popen
 
+from matplotlib import pyplot as plt
+
 from time import time
 from copy import deepcopy
 from glob import glob
@@ -660,7 +662,7 @@ class fgrid(object):				#Grid object.
 			 (len(np.unique([nd.position[2] for nd in self.nodelist])) == 1)):
 			self._dimensions = 2
 		else: self._dimensions = 3
-	def write(self,filename=None,format='fehm', compression = True):
+	def write(self,filename=None,format='fehm', compression = True, recalculate_coefficients=True):
 		"""Write grid object to a grid file (FEHM, AVS STOR file formats supported). Stor file support only for orthogonal hexahedral grids.
 
 		:param filename: name of FEHM grid file to write to, including path specification, e.g. c:\\path\\file_out.inp
@@ -701,7 +703,7 @@ class fgrid(object):				#Grid object.
 		
 		if format == 'fehm': self._write_fehm(outfile)
 		elif format == 'avs': self._write_avs(outfile)
-		elif format == 'stor': self._write_stor(outfile,compression)
+		elif format == 'stor': self._write_stor(outfile,compression,recalculate_coefficients)
 		else: print 'ERROR: Unrecognized format '+format+'.'; return
 		
 		if format == 'stor': 
@@ -761,17 +763,19 @@ class fgrid(object):				#Grid object.
 					outfile.write(str(nd)+'   ')
 				outfile.write('\n')
 		outfile.close()
-	def _write_stor(self,outfile,compression=True):
+	def _write_stor(self,outfile,compression,recalculate_coefficients):
 		# calculate volumes and geometric coefficients
-		for conn in self.connlist: conn._geom_coef = None
-		for nd in self.nodelist: nd._vol = None
-		for nd in self.nodelist:
-			self._vorvol(nd) 		# calculate voronoi volume of node
+		if recalculate_coefficients:
+			for conn in self.connlist: conn._geom_coef = None
+			for nd in self.nodelist: nd._vol = None
+			for nd in self.nodelist:
+				self._vorvol(nd) 		# calculate voronoi volume of node
 		# calculate reduced coefficient list
 		if compression:
 			tol = 1.e-5
 			indices1 = [] 			# for populating later
 			geom_coefs = [con.geom_coef for con in self.connlist] 	# all coefficients
+			geom_coefs = list(flatten(geom_coefs))
 			gcU = np.ones((1,len(geom_coefs)))[0]*float('Inf')
 			NgcU = 0
 			for gc in geom_coefs:
@@ -836,8 +840,8 @@ class fgrid(object):				#Grid object.
 			# add the connections
 			cons = []
 			for con in nd.connections:
-				if con.nodes[0].index == nd.index: cons.append([con.nodes[1].index,con.geom_coef])
-				else: cons.append([con.nodes[0].index,con.geom_coef])
+				if con.nodes[0].index == nd.index: cons.append([con.nodes[1].index,con.geom_coef[0]])
+				else: cons.append([con.nodes[0].index,con.geom_coef[-1]])
 			cons.append([nd.index,0.])
 			cons.sort(key=lambda x: x[0])
 			for j,con in enumerate(cons):
@@ -913,11 +917,14 @@ class fgrid(object):				#Grid object.
 			# establish direction of connection
 			N = abs(con.nodes[0].position - con.nodes[1].position)
 			if N[0]>N[1] and N[0]>N[2]:	
-				con._geom_coef = areas[0]/(con.distance/2.)
+				#con._geom_coef = [areas[0]/(con.distance/2.),]
+				con._geom_coef = [areas[0]/(con.distance),]
 			elif N[1]>N[0] and N[1]>N[2]:	
-				con._geom_coef = areas[1]/(con.distance/2.)
+				#con._geom_coef = [areas[1]/(con.distance/2.),]
+				con._geom_coef = [areas[1]/(con.distance),]
 			elif N[2]>N[1] and N[2]>N[0]:	
-				con._geom_coef = areas[2]/(con.distance/2.)
+				#con._geom_coef = [areas[2]/(con.distance/2.),]
+				con._geom_coef = [areas[2]/(con.distance),]
 	def make(self,gridfilename,x,y,z,full_connectivity=False,octree=False):
 		""" Generates an orthogonal mesh for input node positions. 
 		

fpost.py
@@ -778,7 +778,8 @@ class fcontour(object): 					# Reading and plotting methods associated with cont
 		:type perm_contrasts: bool
 		
 		'''	
-		save = os_path(save)
+		if save:
+			save = os_path(save)
 		# at this stage, only structured grids are supported
 		if time==None: 
 			if np.min(self.times)<0: time = self.times[0]
@@ -860,13 +861,16 @@ class fcontour(object): 					# Reading and plotting methods associated with cont
 					ax.plot([t,t],[ylims[0],ylims[1]],mesh_lines,zorder=100)
 				for t in np.unique(self[self.times[0]]['z']):
 					ax.plot([xlims[0],xlims[1]],[t,t],mesh_lines,zorder=100)
-					
-		extension, save_fname, pdf = save_name(save=save,variable=variable,time=time)
-		plt.savefig(save_fname, dpi=100, facecolor='w', edgecolor='w',orientation='portrait', 
-		format=extension,transparent=True, bbox_inches=None, pad_inches=0.1)
-		if pdf: 
-			os.system('epstopdf ' + save_fname)
-			os.remove(save_fname)	
+		
+		if save:
+			extension, save_fname, pdf = save_name(save=save,variable=variable,time=time)
+			plt.savefig(save_fname, dpi=100, facecolor='w', edgecolor='w',orientation='portrait', 
+			format=extension,transparent=True, bbox_inches=None, pad_inches=0.1)
+			if pdf: 
+				os.system('epstopdf ' + save_fname)
+				os.remove(save_fname)	
+		else:
+			plt.show()
 	def profile(self, variable, profile, time=None, divisions=30, method='nearest'):
 		'''Return variable data along the specified line in 3-D space. If only two points are supplied,
 		the profile is assumed to be a straight line between them.